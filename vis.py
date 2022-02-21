5e5c7a05d8c845534e7c8b13cc87cc6a325adfbf
David Walker <david.walker@plymouth.ac.uk>
2022-02-21 12:44:48 +0000

    Adding helper function to extract Platypus solutions.

--- vis.py
+++ vis.py
@@ -2,3 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def unpack_platypus(optimiser):
+    """
+    Take the Pareto front approximation from a Platypus optimiser and
+    return a tuple of Numpy arrays -- one holds the decision variables and
+    the other hold the objective variables.
+    """
+    X = np.array([soln.variables for soln in optimiser.result])
+    Y = np.array([soln.objectives for soln in optimiser.result])
+    return X, Y
+
+
